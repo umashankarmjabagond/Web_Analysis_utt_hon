c28686849856e46b01a70bd1972385c47d55d13a
Ravi Kiran M <ravi.km@utthunga.com>
2026-08-03 15:54:54 +0530

    Add reusable tooltip

--- src/pages/analysis/template-execution/components/nodes/BaseNode.tsx
+++ src/pages/analysis/template-execution/components/nodes/BaseNode.tsx
@@ -10,6 +10,7 @@ import type {
 import { useWorkflowCanvasInteractions } from "../../../../../hooks/useWorkflowInteractions";
 import { Fragment } from "react/jsx-runtime";
 import type { CSSProperties } from "react";
+import Tooltip from "../../../../../components/common/tooltip/Tooltip";
 
 export default function BaseNode({ id, data, type }: NodeProps<BaseFlowNode>) {
   const checked = useTemplateExecutionStore((state) =>
@@ -87,103 +88,70 @@ export default function BaseNode({ id, data, type }: NodeProps<BaseFlowNode>) {
     pointerEvents: "none",
   };
 
+  const nodeStatusMsg =
+    data.status === "warning"
+      ? "Warning message will be shown here"
+      : data.status === "error"
+        ? "There are too many bad data points"
+        : null;
+
   return (
-    <div
-      className={`group nodrag nopan relative w-20 min-w-20 min-h-22 rounded-[4px] px-2 py-3 overflow-hidden border cursor-pointer ${nodeStyle.background} ${nodeStyle.border}`}
-    >
-      {nodeStyle.tint && (
-        <div
-          className={`absolute inset-0 pointer-events-none ${nodeStyle.tint}`}
-        />
-      )}
-
+    <Tooltip content={nodeStatusMsg} disabled={checked}>
       <div
-        className={`absolute top-1 left-1 z-20 ${
-          checked ? "block" : "hidden group-hover:block"
-        }`}
+        className={`group nodrag nopan relative w-20 min-w-20 min-h-22 rounded-[4px] px-2 py-3 overflow-hidden border cursor-pointer ${nodeStyle.background} ${nodeStyle.border}`}
       >
-        <div className="relative h-4 w-4 flex items-center justify-center">
-          <input
-            type="checkbox"
-            checked={checked}
-            onChange={() => handleNodeSelection(id, data.status)}
-            onClick={(e) => e.stopPropagation()}
-            className="nodrag nopan peer h-4 w-4 appearance-none rounded-xs border border-app-default-border bg-transparent checked:border-app-action-primary checked:bg-app-action-primary cursor-pointer"
+        {nodeStyle.tint && (
+          <div
+            className={`absolute inset-0 pointer-events-none ${nodeStyle.tint}`}
           />
+        )}
 
-          <Check
-            className="pointer-events-none absolute h-3 w-3 text-black opacity-0 peer-checked:opacity-100"
-            strokeWidth={3}
-          />
+        <div
+          className={`absolute top-1 left-1 z-20 ${
+            checked ? "block" : "hidden group-hover:block"
+          }`}
+        >
+          <div className="relative h-4 w-4 flex items-center justify-center">
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={() => handleNodeSelection(id, data.status)}
+              onClick={(e) => e.stopPropagation()}
+              className="nodrag nopan peer h-4 w-4 appearance-none rounded-xs border border-app-default-border bg-transparent checked:border-app-action-primary checked:bg-app-action-primary cursor-pointer"
+            />
+
+            <Check
+              className="pointer-events-none absolute h-3 w-3 text-black opacity-0 peer-checked:opacity-100"
+              strokeWidth={3}
+            />
+          </div>
         </div>
+
+        <div className="relative z-10 flex h-full flex-col items-center justify-center gap-2">
+          <Icon size={16} className="shrink-0 text-app-text-secondary" />
+          <span className="w-full h-10 line-clamp-2 text-center text-sm text-app-action-secondary leading-5">
+            {data.label}
+          </span>
+        </div>
+
+        {HANDLE_CONFIG.map((handle) => (
+          <Fragment key={handle.id}>
+            <Handle
+              id={handle.id}
+              type="target"
+              position={handle.position}
+              style={handleStyle}
+            />
+
+            <Handle
+              id={handle.id}
+              type="source"
+              position={handle.position}
+              style={handleStyle}
+            />
+          </Fragment>
+        ))}
       </div>
-
-      {/* Target Handles */}
-      {/* <Handle
-        type="target"
-        position={Position.Left}
-        style={{
-          visibility: "hidden",
-          opacity: 0,
-          pointerEvents: "none",
-        }}
-      />
-
-      <Handle
-        type="target"
-        position={Position.Top}
-        style={{
-          visibility: "hidden",
-          opacity: 0,
-          pointerEvents: "none",
-        }}
-      /> */}
-
-      <div className="relative z-10 flex h-full flex-col items-center justify-center gap-2">
-        <Icon size={16} className="shrink-0 text-app-text-secondary" />
-        <span className="w-full h-10 line-clamp-2 text-center text-sm text-app-action-secondary leading-5">
-          {data.label}
-        </span>
-      </div>
-
-      {HANDLE_CONFIG.map((handle) => (
-        <Fragment key={handle.id}>
-          <Handle
-            id={handle.id}
-            type="target"
-            position={handle.position}
-            style={handleStyle}
-          />
-
-          <Handle
-            id={handle.id}
-            type="source"
-            position={handle.position}
-            style={handleStyle}
-          />
-        </Fragment>
-      ))}
-
-      {/* Source Handles */}
-      {/* <Handle
-        type="source"
-        position={Position.Right}
-        style={{
-          visibility: "hidden",
-          opacity: 0,
-          pointerEvents: "none",
-        }}
-      />
-
-      <Handle
-        type="source"
-        position={Position.Bottom}
-        style={{
-          visibility: "hidden",
-          opacity: 0,
-          pointerEvents: "none",
-        }}
-      /> */}
-    </div>
+    </Tooltip>
   );
 }
