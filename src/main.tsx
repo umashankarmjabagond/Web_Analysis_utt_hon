6f26132446b73d44c9cb6a98cbd8566501e2dccb
Ravi Kiran M <ravi.km@utthunga.com>
2026-08-12 10:39:11 +0530

    Apply index.css

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./i18n";
 import App from "./App";
-// import "./index.css";
+import "./index.css";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
