a72ab5f0e66410d4cac95fa78bfe1d0bc2b34aa3
Ravi Kiran M <ravi.km@utthunga.com>
2026-08-19 15:00:26 +0530

    Add test cases for canvas page

--- src/pages/analysis/template-execution/components/nodes/ExecutionRowNode.tsx
+++ src/pages/analysis/template-execution/components/nodes/ExecutionRowNode.tsx
@@ -19,7 +19,7 @@ export default function ExecutionRowNode({
     <div
       data-testid="execution-row"
       className={cn(
-        "nodrag nopan flex h-full w-full items-start py-1.5 px-3 gap-3 rounded-[8px] border",
+        "nodrag nopan cursor-default flex h-full w-full items-start py-1.5 px-3 gap-3 rounded-[8px] border",
         checked
           ? "border-[#4FB3FF] bg-[#4FB3FF29]"
           : "border-[#454545] bg-[#1B1B1B]",
